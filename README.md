b2d1bc315a28ffe6d12502591d3072983d7fd937
mihiretab-taye <mihiretab.taye@gmail.com>
2022-01-16 13:35:27 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Automatic Mining Excutor
+
+## How To Run?
+
+### Make sure you have installed python
+
+### Download apropriate driver for your browser according to your version
+
+[For Chrome](https://chromedriver.chromium.org/downloads)
+[For Firefox](https://github.com/mozilla/geckodriver/releases)
+
+### After downloading the driver extract and put the exe file inside the driver folder
+
+- clone this repo
+- cd automatic-click
+- python -m venv venv
+- venv\Scripts\activate
+- python -m pip install -r requirements.txt
+- python script.py
+- Enjoy!
+
+## Thank You
+
+- Author : Mihiretab Taye
---
+++ script.py
@@ -0,0 +1,39 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+
+  
+while(True):
+    # use appropriate driver for your browser
+    driver = webdriver.Chrome(r'.\driver\chromedriver.exe')
+
+    link = "http://localhost:15000/docs/index.html"
+    driver.get(link)
+    
+    driver.maximize_window()
+    time.sleep(1)
+    
+
+    post = driver.find_element(By.ID, "operations-Mining-post_api_Mining_generate")
+    post.click()
+
+
+    try_it_out = driver.find_element(By.XPATH, "//button[@class='btn try-out__btn']")
+    try_it_out.click()
+
+    parameter = driver.find_element(By.XPATH, "//textarea[@class='body-param__text']")
+    parameter.clear()
+    
+    # use your prefered block count
+    parameter.send_keys('{\n  "blockCount": 10000}\n}')
+
+    execute = driver.find_element(By.XPATH, "//div[@class='execute-wrapper']/button")
+    execute.click()
+    time.sleep(3)
+    
+    # close browser
+    driver.quit()
+    
+    # set the interval to trigger mine command with your prefered time
+    # e.g it waits for 30 minutes and trigger again
+    time.sleep(1800)
